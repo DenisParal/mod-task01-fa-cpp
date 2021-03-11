5be57795c3e395062c9a0d7848896d36183f2d15
dparanic <den.par.al@yandex.com>
2021-03-11 16:26:37 +0300

    Paranichev Denis: 381807-1

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,12 +1,79 @@
-unsigned int faStr1(const char *str)
-{
-    return 0;
+unsigned int faStr1(const char *str){
+    long long index=0;
+    unsigned int counter=0;
+    bool is_word=false;
+    bool is_numeric=false;
+    while(str[index-1]!='\0') {
+        if(str[index]==' ' || str[index]=='\0'){ // Any symbol except space and EOL
+            if(is_word && !is_numeric){
+                ++counter;
+            }
+            is_word=false;
+            is_numeric=false;
+        }else{
+            if(str[index]>47 && str[index]<58){ // integers in ASCII
+                is_numeric=true;
+            }
+            is_word=true;
+        }
+        ++index;
+    }
+    return counter;
 }
-unsigned int faStr2(const char *str)
-{
-    return 0;
+
+unsigned int faStr2(const char *str){
+    long long index=0;
+    unsigned int counter=0;
+    bool is_word=false;
+    bool has_upper=false;
+    while(str[index-1]!='\0') {
+        if(str[index]==' ' || str[index]=='\0'){ // Any symbol except space and EOL
+            if(is_word && !has_upper){
+                ++counter;
+            }
+            is_word=false;
+            has_upper=false;
+        }else{
+            if(str[index]>64 && str[index]<91){ // Upper case letters in ASCII
+                if(index==0 || index>0 && str[index-1]==' '){
+                    is_word=true;
+                }else{
+                    has_upper=true;
+                }
+            }else{
+                if(!(str[index]>64 && str[index]<91) && !(str[index]>96 && str[index]<123)){ // Any letter in ASCII
+                    is_word=false;
+                }
+            }
+        }
+        ++index;
+    }
+    return counter;
 }
-unsigned int faStr3(const char *str)
-{
-    return 0;
+
+unsigned int faStr3(const char *str){
+    long long index=0;
+    unsigned int counter=0;
+    unsigned int symbol_counter=0;
+    bool is_word=false;
+    while(str[index-1]!='\0') {
+        if(str[index]==' ' || str[index]=='\0'){ // Any symbol except space and EOL
+            if(is_word){
+                ++counter;
+            }
+            is_word=false;
+        }else{
+            is_word=true;
+            ++symbol_counter;
+        }
+        ++index;
+    }
+    if(counter==0){
+        return 0;
+    }
+    if(((symbol_counter*10)/counter)%10>=5){
+        return symbol_counter/counter+1;
+    }else{
+        return symbol_counter/counter;
+    }
 }
